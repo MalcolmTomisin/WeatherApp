f6b2334231066bed91aa3abe1388fb0db12e4747
Oluwatomisin <malcolmtomisin@gmail.com>
2020-09-23 17:49:24 +0100

    underlying project constants

---
+++ constants/Cities.js
@@ -0,0 +1,4 @@
+export const CITIES = ['Beijing', 'Buenos Aires', 'Cairo', 'Chongqing',
+    'Delhi', 'Dhaka', 'Istanbul', 'Karachi', 'Kolkata', 'Mexico City', 'Mumbai',
+    'Osaka', 'Sao Paulo', 'Shangai', 'Tokyo'
+];
