aa5debdb023c2c1a14ed0bf28ae395ea21eed4c9
Oluwatomisin <malcolmtomisin@gmail.com>
2020-09-24 00:53:51 +0100

    refactor fetch function

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 
-export function useFetch(url) {
+export function useFetch(url, param = null) {
     const [data, setData] = useState();
     const [error, setError] = useState();
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         if (!url) return;
-        fetch(url)
-            .then(res => res.json())
-            .then(setData)
-            .catch(setError)
-            .finally(() => setLoading(false));
+        if (param) {
+            fetch(url, param)
+              .then((res) => res.json())
+              .then(setData)
+              .catch(setError)
+              .finally(() => setLoading(false));
+        }
+        else {
+            fetch(url)
+              .then((res) => res.json())
+              .then(setData)
+              .catch(setError)
+              .finally(() => setLoading(false));
+        }
     }, [url]);
     
     return {
