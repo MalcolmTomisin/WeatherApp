766f77b848b62e7ad7fd76b505398652f237a0d4
Oluwatomisin <malcolmtomisin@gmail.com>
2020-09-24 13:45:43 +0100

    implement weather list component

--- components/List.jsx
+++ components/List.jsx
@@ -4,19 +4,27 @@ import Icons from './Icons';
 import Text from './Text';
 
 
-export default function (props) {
+export default function ListItem (props) {
     const _renderItem = ({ item }) => (
       <View>
         <View>
-          <Text></Text>
-                <Icons uri={item.weather_icons[0]} />
-            </View>
-            <View>
-                <Text></Text>
-                <View>
-
-                </View>
-            </View>
+          <Text>{item.city}</Text>
+          <Icons uri={item.weather_icons[0]} />
+        </View>
+        <View>
+          <Text>{`${item.temp}°C`}</Text>
+        </View>
       </View>
-    );
+  );
+  
+  const _keyExtractor = (item, index) => item.id;
+
+  return (
+    <FlatList
+      data={props.data}
+      keyExtractor={_keyExtractor}
+      renderItem={_renderItem}
+    />
+  );
+
 }
