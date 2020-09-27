57b53c3f856fe2462b3406b2a8f131bbc7171606
Oluwatomisin <malcolmtomisin@gmail.com>
2020-09-27 13:05:50 +0100

    cache resources

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import {ActivityIndicator} from 'react-native';
 import Navigation from './navigation';
 import { Provider } from "react-redux";
-import store,{persistor} from './redux/store';
+import store, { persistor } from './redux/store';
+import useCachedResources from "./hooks/useCachedResources";
 import { PersistGate } from "redux-persist/integration/react";
 
 const App: () => React$Node = () => {
+  const isLoadingComplete = useCachedResources();
+  if (!isLoadingComplete) {
+    return null;
+  }
+  
   return (
     <Provider store={store}>
       <PersistGate
